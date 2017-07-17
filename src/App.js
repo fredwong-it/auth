9b7be0c2c582f8b2f9d766d83c07bdeb2fab6407
Fred Wong <fredwong.it@gmail.com>
2017-07-17 13:11:57 -0400

    Add eslint and firebase client setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Header } from './components/common'
+import firebase from 'firebase';
+import { Header } from './components/common';
 
 class App extends Component {
+  // similar to ngOnInit in Angular
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBFHHjHf2y7xaPdhmrOQe8AChgc12UD_T4',
+      authDomain: 'auth-426a6.firebaseapp.com',
+      databaseURL: 'https://auth-426a6.firebaseio.com',
+      projectId: 'auth-426a6',
+      storageBucket: 'auth-426a6.appspot.com',
+      messagingSenderId: '140381376292'
+    });
+  }
+
   render() {
     return (
       <View>
