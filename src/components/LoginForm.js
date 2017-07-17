c5ec986eee67936c0d04be309a2056d845b5e10f
Fred Wong <fredwong.it@gmail.com>
2017-07-17 17:39:42 -0400

    Add style to Input component and finish the user input

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,25 +3,20 @@ import { Text, TextInput } from 'react-native';
 import { Card, CardSection, Button, Input } from './common';
 
 class LoginForm extends Component {
-  state = { text: '', password: '' };
+  state = { email: '', password: '' };
 
   render() {
     return (
       <Card>
         <CardSection>
-          <Text>Email:</Text>
-          <TextInput
-            value={this.state.text}
-            onChangeText={text => this.setState({ text })}
-            style={{ height: 20, width: 100 }}
+          <Input
+            label='Email'
+            placeholder='user@gmail.com'
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
           />
         </CardSection>
         <CardSection>
-          <Input
-            label='Password'
-            value={this.state.password}
-            onChangeText={password => this.setState({ password })}
-          />
         </CardSection>
         <CardSection>
           <Button>Log in</Button>
