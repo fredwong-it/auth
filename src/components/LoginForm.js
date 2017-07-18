eef9c1363d9398087733db13b9797a977911870b
Fred Wong <fredwong.it@gmail.com>
2017-07-18 11:34:26 -0400

    Create the Spinner component and add it to the screen

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
 import firebase from 'firebase';
-import { Card, CardSection, Button, Input } from './common';
+import { Card, CardSection, Button, Input, Spinner } from './common';
 
 class LoginForm extends Component {
-  state = { email: '', password: '', error: '' };
+  state = {
+    email: '',
+    password: '',
+    error: '',
+    loading: false
+  };
 
   onButtonPress() {
     const { email, password } = this.state;
 
-    this.setState({ error: '' });
+    this.setState({ error: '', loading: true });
 
     firebase.auth().signInWithEmailAndPassword(email, password)
       .catch(() => {
         firebase.auth().createUserWithEmailAndPassword(email, password)
           .catch(() => {
-            this.setState({ error: 'Authentication Failed!' });
+            this.setState({ error: 'Authentication Failed!', loading: false });
           });
       });
+  }
+
+  renderButton() {
+    if (this.state.loading) {
+      return <Spinner size="small" />;
+    }
+
+    return (
+      <Button onPress={this.onButtonPress.bind(this)}>
+        Log in
+      </Button>
+    );
   }
 
   render() {
@@ -47,7 +64,7 @@ class LoginForm extends Component {
         </Text>
 
         <CardSection>
-          <Button onPress={this.onButtonPress.bind(this)}>Log in</Button>
+          {this.renderButton()}
         </CardSection>
       </Card>
     );
